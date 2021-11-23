cdcb87ea6534483f9d13a4e3e498bdd2f7a1904b
Glitch (sugared-cyclic-peanut) <none>
2021-11-23 23:43:16 +0000

    💠💂 Checkpoint ./script.js:35891473/40 ./style.css:35891362/223:35891473/251 ./index.html:35891362/576

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 /*Main JavaScript. Dan Moraru & Marko Litovchenko*/
 
 console.log("Dan won't notice this until novemeber");
-//I noticed (cap) bruh what you mean cap???
+//I noticed (cap) bruh what you mean cap??? u didnt notice this before november
