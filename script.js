f603a82b2093f0b607d3fc790c86bba100330354
Glitch (sugared-cyclic-peanut) <none>
2021-12-01 01:28:33 +0000

    Rewound to commit 4f0ffd1c026cfc8190cce0009f89dfb3df084401 + 0 edits

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 /*Main JavaScript. Dan Markaru & Marko Litovchenko*/
 
-let link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = '/dark.css';
-    document.head.appendChild(link);
+/*If marko sees this hes gay*/
 
 let newsLetter = document.getElementById("email");
-let logo = document.getElementById("logoHead");
-let orderButton = document.getElementById("insideButton");
-let switchThemes = document.getElementById("switch");
 
-let themeButton = document.getElementById("switch");
-let themeLink = document.getElementsByTagName("link")[2];
+let orderButton = document.getElementById("insideButton");
 
 newsLetter.addEventListener("click", function(){
   alert("Thank you for Subscribing!");
@@ -20,9 +13,3 @@ newsLetter.addEventListener("click", function(){
 orderButton.addEventListener("click", function(){
   window.location.replace("order.html");
 })
-
-themeButton.addEventListener("click", function(){
-  themeLink.href = "/light.css";
-  
-})
-
