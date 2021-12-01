c82fea1b1c0b9088a58d5b897278cb029b17822e
Glitch (sugared-cyclic-peanut) <none>
2021-12-01 01:59:59 +0000

    💠🌓 Checkpoint ./index.html:35891473/14 ./test.js:35891473/234 ./position.css:35891362/21 ./dark.css:35891362/12

--- test.js
+++
@@ -1,8 +0,0 @@
-let themeButton = document.getElementById("switch");
-let themeLink = document.getElementsByTagName("link")[2];
-
-themeButton.addEventListener("click", function(){
-  themeLink.href = "/light.css";
-  
-})
-
