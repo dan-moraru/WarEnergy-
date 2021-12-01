e3b47f71c08b22892ea767b8e38db0d8af87d7aa
Glitch (sugared-cyclic-peanut) <none>
2021-12-01 18:18:05 +0000

    🏋💷 Checkpoint ./script.js:35891473/883 ./aboutus.html:35891473/461 ./artistroom.html:35891473/461 ./quiz.html:35891473/470 ./recipe.html:35891473/461

--- script.js
+++ script.js
@@ -3,15 +3,19 @@
 
 
 let newsLetter = document.getElementById("email");
-let logo = document.getElementById("logoHead");
-let orderButton = document.getElementById("insideButton");
+let logoHead = document.getElementById("logoHead");
+let logoFoot = document.getElementById("logoFoot");
 
+let orderButton = document.getElementById("insideButton");
 let themeButton = document.getElementById("switch");
 let themeLink = document.getElementsByTagName("link")[1];
 
 newsLetter.addEventListener("click", function(){
   alert("Thank you for Subscribing!");
 })
+
+//This code gives errors on other pages as the orderButton exists only in order.html, 
+//when they same script is attached to every page
 
 /*orderButton.addEventListener("click", function(){
   window.location.replace("order.html");
@@ -20,10 +24,13 @@ newsLetter.addEventListener("click", function(){
 themeButton.addEventListener("click", function(){
   if (themeLink.getAttribute("href") == "/dark.css"){
     themeLink.setAttribute("href", "/light.css");
-    
+    logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
+    logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
     themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2F183-1836881_half-moon-png-pic-half-moon-logo-png.png?v=1638314118399");
   }else if (themeLink.getAttribute("href") == "/light.css"){
     themeLink.setAttribute("href", "/dark.css");
+    logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+    logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
     themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FUntitled-1.png?v=1638322301896")
   }
 
