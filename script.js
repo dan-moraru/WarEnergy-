43f5b8dd4ddb9afc841dbb353f82b92e9ddb93b4
Glitch (sugared-cyclic-peanut) <none>
2021-12-01 01:30:18 +0000

    🏙🏙 Checkpoint ./script.js:35891473/337

--- script.js
+++ script.js
@@ -5,11 +5,16 @@
 let newsLetter = document.getElementById("email");
 
 let orderButton = document.getElementById("insideButton");
-
+let themeButton = document.getElementById("switch");
+let cssLink = document.getElementsByTagName("link")[2];
 newsLetter.addEventListener("click", function(){
   alert("Thank you for Subscribing!");
 })
 
 orderButton.addEventListener("click", function(){
   window.location.replace("order.html");
+})
+
+themeButton.addEventListener("click", function(){
+  cssLink.href = "/light.css";
 })
