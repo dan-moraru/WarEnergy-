b477020e73d5832f3a30158dcdb5a3842af7a2ad
Glitch (sugared-cyclic-peanut) <none>
2021-12-02 15:28:10 +0000

    🎇🛩 Checkpoint ./quiz.js:35891473/337 ./quiz.html:35891473/19

--- quiz.js
+++ quiz.js
@@ -1,42 +1,42 @@
-let div1 = document.getElementById("div1");
-let div2 = document.getElementById("div2");
-let div3 = document.getElementById("div3");
-let div4 = document.getElementById("div4");
+let firstQuizPage = document.getElementById("firstQuizPage");
+let secondQuizPage = document.getElementById("secondQuizPage");
+/*let div3 = document.getElementById("div3");
+let div4 = document.getElementById("div4");*/
 
-let q1 = document.getElementById("q1");
-let q2 = document.getElementById("q2");
-let q3 = document.getElementById("q3");
-let q4 = document.getElementById("q4");
+let question1 = document.getElementById("topLeftQuestion");
+let question2 = document.getElementById("topRightQuestion");
+let question3 = document.getElementById("bottomLeftQuestion");
+let question4 = document.getElementById("bottomRightQuestion");
 
-/*div1.style.display = "none";
-div2.style.display = "none";
-div3.style.display = "none";
-div4.style.display = "none";
+question1.style.display = "block";
+question2.style.display = "none";
+question3.style.display = "none";
+question4.style.display = "none";
 
-q1.addEventListener("click", function(){
+question1.addEventListener("click", function(){
   div1.style.display = "block";
   div2.style.display = "none";
   div3.style.display = "none";
   div4.style.display = "none";
 })
-q2.addEventListener("click", function(){
+question2.addEventListener("click", function(){
   div1.style.display = "none";
   div2.style.display = "block";
   div3.style.display = "none";
   div4.style.display = "none";
 })
-q3.addEventListener("click", function(){
+question3.addEventListener("click", function(){
   div1.style.display = "none";
   div2.style.display = "none";
   div3.style.display = "block";
   div4.style.display = "none";
 })
-q4.addEventListener("click", function(){
+question4.addEventListener("click", function(){
   div1.style.display = "none";
   div2.style.display = "none";
   div3.style.display = "none";
   div4.style.display = "block";
-})*/
+})
 
 /*let input = document.getElementByid("input1");
 let inputValue = input.getAttribute("value");
