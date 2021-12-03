036942b0079190c9f5f00a19b851e68361e93a4c
Glitch (sugared-cyclic-peanut) <none>
2021-12-03 15:55:01 +0000

    🐿🌲 Checkpoint ./script.js:35891473/2652

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ let themeLink = document.getElementsByTagName("link")[1];
 
 if (myStorage.getItem("themeLink") == "/dark.css"){
   myStorage.setItem("themeLink", "/light.css");
-  myStorage.setItem("logoFoot", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
-  myStorage.setItem("logoHead", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+  myStorage.setItem("logoFoot", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
+  myStorage.setItem("logoHead", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
   myStorage.setItem("themeButton", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2F183-1836881_half-moon-png-pic-half-moon-logo-png.png?v=1638314118399")
   
   themeLink.setAttribute("href", myStorage.getItem("themeLink"));
@@ -38,24 +38,32 @@ if (myStorage.getItem("themeLink") == "/dark.css"){
   themeButton.setAttribute("src", myStorage.getItem("themeButton"));
 }
 
-themeLink.setAttribute("href", myStorage.getItem("themeLink"));
+/*themeLink.setAttribute("href", myStorage.getItem("themeLink"));
 logoFoot.setAttribute("src", myStorage.getItem("logoFoot"));
 logoHead.setAttribute("src", myStorage.getItem("logoHead"));
-themeButton.setAttribute("src", myStorage.getItem("themeButton"));
+themeButton.setAttribute("src", myStorage.getItem("themeButton"));*/
 
 themeButton.addEventListener("click", function(){
   if (myStorage.getItem("themeLink") == "/dark.css"){
     myStorage.setItem("themeLink", "/light.css");
+    myStorage.setItem("logoFoot", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
+    myStorage.setItem("logoHead", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
+    myStorage.setItem("themeButton", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2F183-1836881_half-moon-png-pic-half-moon-logo-png.png?v=1638314118399")
+    
     themeLink.setAttribute("href", myStorage.getItem("themeLink"));
-    logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
-    logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
-    themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2F183-1836881_half-moon-png-pic-half-moon-logo-png.png?v=1638314118399");
+    logoHead.setAttribute("src", myStorage.getItem("logoHead"));
+    logoFoot.setAttribute("src", myStorage.getItem("logoFoot"));
+    themeButton.setAttribute("src", myStorage.getItem("themeButton"));
   }else if (myStorage.getItem("themeLink") == "/light.css"){
     myStorage.setItem("themeLink", "/dark.css");
+    myStorage.setItem("logoFoot", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+    myStorage.setItem("logoHead", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+    myStorage.setItem("themeButton", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FUntitled-1.png?v=1638322301896");
+    
     themeLink.setAttribute("href", myStorage.getItem("themeLink"));
-    logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
-    logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
-    themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FUntitled-1.png?v=1638322301896")
+    logoHead.setAttribute("src", myStorage.getItem("logoHead"));
+    logoFoot.setAttribute("src", myStorage.getItem("logoFoot"));
+    themeButton.setAttribute("src", myStorage.getItem("themeButton"));
   }
 
 })
