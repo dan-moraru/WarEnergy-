1389d8228eea9b4a8f3bbb14efdcfc509df11292
Glitch (sugared-cyclic-peanut) <none>
2021-12-03 15:43:54 +0000

    💼👷 Checkpoint ./script.js:35891473/3442 ./dark.css:35891362/121 ./light.css:35891362/138 ./position.css:35891362/57 ./recipe.html:35891362/6

--- script.js
+++ script.js
@@ -15,22 +15,33 @@ let themeLink = document.getElementsByTagName("link")[1];
   alert("Thank you for Subscribing!");
 })*/
 
-let themeButtonStorage = myStorage.getItem("themeButton");
-
-if (myStorage.getItem("themeLink") == "/light.css"){
-  logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
-  logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
-  themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FUntitled-1.png?v=1638322301896")
-}else if(myStorage.getItem("themeLink") == "/dark.css"){
-  logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
-  logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
-  themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2F183-1836881_half-moon-png-pic-half-moon-logo-png.png?v=1638314118399");
+if (myStorage.getItem("themeLink") == "/dark.css"){
+  myStorage.setItem("themeLink", "/light.css");
+  myStorage.setItem("logoFoot", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+  myStorage.setItem("logoHead", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+  myStorage.setItem("themeButton", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2F183-1836881_half-moon-png-pic-half-moon-logo-png.png?v=1638314118399")
+  
+  themeLink.setAttribute("href", myStorage.getItem("themeLink"));
+  logoFoot.setAttribute("src", myStorage.getItem("logoFoot"));
+  logoHead.setAttribute("src", myStorage.getItem("logoHead"));
+  themeButton.setAttribute("src", myStorage.getItem("themeButton"));
+  
+}else if(myStorage.getItem("themeLink") == "/light.css"){
+  myStorage.setItem("themeLink", "/dark.css");
+  myStorage.setItem("logoFoot", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+  myStorage.setItem("logoHead", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
+  myStorage.setItem("themeButton", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FUntitled-1.png?v=1638322301896")
+  
+  themeLink.setAttribute("href", myStorage.getItem("themeLink"));
+  logoFoot.setAttribute("src", myStorage.getItem("logoFoot"));
+  logoHead.setAttribute("src", myStorage.getItem("logoHead"));
+  themeButton.setAttribute("src", myStorage.getItem("themeButton"));
 }
 
 themeLink.setAttribute("href", myStorage.getItem("themeLink"));
 logoFoot.setAttribute("src", myStorage.getItem("logoFoot"));
 logoHead.setAttribute("src", myStorage.getItem("logoHead"));
-themeButtonStorage.setAttribute("src", myStorage.getItem("themeButton"));
+themeButton.setAttribute("src", myStorage.getItem("themeButton"));
 
 themeButton.addEventListener("click", function(){
   if (myStorage.getItem("themeLink") == "/dark.css"){
