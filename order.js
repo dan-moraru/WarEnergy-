ab0ad95b64b634ae10e5da230c31bab20524166e
Glitch (sugared-cyclic-peanut) <none>
2021-12-03 22:14:16 +0000

    👡👨‍👨‍👦 Checkpoint ./order.js:35891473/147

--- order.js
+++ order.js
@@ -17,24 +17,27 @@ let total = 0;
 
 let originalAndSugarfreeMultiplier = 1.2;
 let boostAndCoffeeMultiplier =  1.7;
+let packChosenMultiplier;
 
-packOf12 = 15.99;
-packOf48 = 35.99;
+let packOf12Price = 15.99;
+let packOf48Price = 35.99;
 
 packOf12.addEventListener("click", function(){
-  total += packOf12;
+  total = packOf12Price;
+  packChosenMultiplier = packOf12;
 })
 
 packOf48.addEventListener("click", function(){
-  total += packOf48;
+  total = packOf48;
 })
 
 originalAndSugarfree.addEventListener("click", function(){
-  total += originalAndSugarfreeMultiplier;
+  total = originalAndSugarfreeMultiplier;
 })
 
 boostAndCoffee.addEventListener("click", function(){
-  total += boostAndCoffeeMultiplier;
+  total = boostAndCoffeeMultiplier;
+  total (price * mulitplier)
 })
 
 /*function calculatePrice(){
