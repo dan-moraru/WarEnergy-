670adedc7aa7f5142824124cd985136fe08cf877
Glitch (sugared-cyclic-peanut) <none>
2021-12-03 22:36:28 +0000

    🏯🚔 Checkpoint ./order.js:35891362/42:35891473/1199 ./order.html:35891473/43

--- order.js
+++ order.js
@@ -9,22 +9,27 @@ let boostAndCoffee = document.getElementById("hot");
 let packOf12 = document.getElementById("12pack");
 let packOf48 = document.getElementById("48pack");
 
-let taxes = 14.975;
+let taxes = 1.14975;
 let total = 0;
 
 let originalAndSugarfreeMultiplier = 1.2;
 let boostAndCoffeeMultiplier =  1.7;
-let packChosenMultiplier;
 
 let packOf12Price = 15.99;
 let packOf48Price = 35.99;
-let packChosenPrice;
-let packChoseMultiplier;
+
+let packChosenPrice = 0;
+let packChoseMultiplier = 0;
 
 let totalWithoutTaxes = document.getElementById("total1");
 let totalWithTaxes = document.getElementById("total2");
 
 let purchaseForm = document.getElementById("purchase");
+
+total = packChosenPrice * packChoseMultiplier;
+totalWithoutTaxes.textContent = total + "$";
+totalWithTaxes = total * taxes;
+totalWithTaxes.textContent = totalWithTaxes + "$";
 
 packOf12.addEventListener("click", function(){
   packChosenPrice = packOf12Price;
@@ -42,11 +47,30 @@ boostAndCoffee.addEventListener("click", function(){
   packChoseMultiplier = boostAndCoffeeMultiplier;
 })
 
-purchaseForm.on("change", function(){
+packOf12.addEventListener("change", function(){
   total = packChosenPrice * packChoseMultiplier;
-  totalWithoutTaxes.textContent = total;
+  totalWithoutTaxes.textContent = total + "$";
+  totalWithTaxes = total * taxes;
+  totalWithTaxes.textContent = totalWithTaxes + "$";
 });
-
+packOf48.addEventListener("change", function(){
+  total = packChosenPrice * packChoseMultiplier;
+  totalWithoutTaxes.textContent = total + "$";
+  totalWithTaxes = total * taxes;
+  totalWithTaxes.textContent = totalWithTaxes + "$";
+});
+originalAndSugarfree.addEventListener("change", function(){
+  total = packChosenPrice * packChoseMultiplier;
+  totalWithoutTaxes.textContent = total + "$";
+  totalWithTaxes = total * taxes;
+  totalWithTaxes.textContent = totalWithTaxes + "$";
+});
+boostAndCoffee.addEventListener("change", function(){
+  total = packChosenPrice * packChoseMultiplier;
+  totalWithoutTaxes.textContent = total + "$";
+  totalWithTaxes = total * taxes;
+  totalWithTaxes.textContent = totalWithTaxes + "$";
+});
 
 
 /*function calculatePrice(){
