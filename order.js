649ce36d93d2c5820f065122e94509394a90ecad
Glitch (sugared-cyclic-peanut) <none>
2021-12-03 17:28:30 +0000

    🏣🐫 Checkpoint ./order.html:35891473/12 ./order.js:35891362/93:35891473/1032

---
+++ order.js
@@ -0,0 +1,22 @@
+/*Created by Marko Litovchenko and Dan Moraru*/
+//12 pack = 15.99
+//48 pack = 35.99
+//taxes 14.975%
+let originalAndSugarfree = document.getElementById("cold");
+let boostAndCoffee = document.getElementById("hot");
+
+let packOf12 = document.getElementById("12pack");
+let packOf48 = document.getElementById("48pack");
+
+let total = 0;
+
+let originalAndSugarfreeMultiplier = 1.2;
+let boostAndCoffeeMultiplier =  1.7;
+
+function getPrice(){
+  //if (input.name = packof12)
+}
+
+function caluclateTaxes(){
+  
+}
