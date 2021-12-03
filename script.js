9575187b6f51e6d7b7ed861845bb106bc01b4f1f
Glitch (sugared-cyclic-peanut) <none>
2021-12-03 02:32:41 +0000

    🌼🚨 Checkpoint ./script.js:35891362/46:35891473/792 ./position.css:35891362/263 ./quiz.html:35891362/624

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*Main JavaScript. Dan Markaru & Marko Litovchenko*/
 
-/*let myStorage = window.localStorage;
-let lightTheme = myStorage.getItem("/light.css");*/
+let myStorage = window.localStorage;
+let lightTheme = myStorage.getItem("/light.css");
 
 let newsLetter = document.getElementById("email");
 let logoHead = document.getElementById("logoHead");
@@ -15,23 +15,22 @@ newsLetter.addEventListener("click", function(){
   alert("Thank you for Subscribing!");
 })
 
-//This code gives errors on other pages as the orderButton exists only in order.html, 
-//when they same script is attached to every page
+myStorage.setItem("themeLink", "/dark.css");
 
 /*orderButton.addEventListener("click", function(){
   window.location.replace("order.html");
 })*/
 
 themeButton.addEventListener("click", function(){
-  if (themeLink.getAttribute("href") == "/dark.css"){
-    //myStorage.setItem("themeLink", "/light.css");
-    themeLink.setAttribute("href", "/light.css");
+  if (myStorage.getItem("themeLink") == "/dark.css"){
+    myStorage.setItem("themeLink", "/light.css");
+    themeLink.setAttribute("href", myStorage);
     logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
     logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoPNG.png?v=1637714448522");
     themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2F183-1836881_half-moon-png-pic-half-moon-logo-png.png?v=1638314118399");
-  }else if (themeLink.getAttribute("href") == "/light.css"){
-    //myStorage.setItem("themeLink", "/dark.css");
-    themeLink.setAttribute("href", "/dark.css");
+  }else if (myStorage.getItem("themeLink") == "/light.css"){
+    myStorage.setItem("themeLink", "/dark.css");
+    themeLink.setAttribute("href", myStorage.getItem("themeLink"));
     logoHead.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
     logoFoot.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FwebLogoWhitePNG.png?v=1638310524846");
     themeButton.setAttribute("src", "https://cdn.glitch.me/536f4a60-7027-4969-bb7b-dc4f746a8da3%2FUntitled-1.png?v=1638322301896")
